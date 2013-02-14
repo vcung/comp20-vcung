a2296422e3800d72d0b31ecf0d3cc4496af6a2f8
Vivian Cung <vivian.cung@tufts.edu>
2013-02-14 09:13:41 -0500

    cropped ou mspacman and a ghost, fixed the background

--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -1,5 +1,5 @@
 function draw() {
-    canvas = document.getElementById("simple");
+    canvas = document.getElementById("bg");
         // Check if canvas is supported on browser
     if (canvas.getContext) {
         ctx = canvas.getContext('2d');
@@ -7,9 +7,12 @@ function draw() {
         img.src = "pacman10-hp-sprite.png";
         img.onload = function () {
 		ctx.drawImage(img, 321, 1, 465, 136, 0, 0, 786, 280);
+		ctx.drawImage(img, 99, 60, 21, 17, 4, 30, 35, 30);
+		ctx.drawImage(img, 101, 142, 17, 16, 9, 50, 25, 25);
+		
         window.onload.draw();
         }
-	}
+	} 
     else {
         alert('Sorry, canvas is not supported on your browser!');
     }
