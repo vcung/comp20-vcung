e8a6a7a3df8247d1760a5f56307f50e261f78a84
Vivian Cung <vivian.cung@tufts.edu>
2013-03-07 00:16:45 -0500

    added readme and station_info.js, updated files to show the locationof Carmen and Waldo, and updated redline station information

--- where/map.js
+++ where/map.js
@@ -5,14 +5,15 @@ function init() {
     request = new XMLHttpRequest();
     var me = new google.maps.LatLng(myLat, myLong);
     var myOptions = {
-zoom: 13, // The larger the zoom number, the bigger the zoom
-      center: landmark,
+      zoom: 13,
+      center: me,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
     infowindow = new google.maps.InfoWindow();
     getLocation();
+    findWaldoCarmen();
 }
 function getLocation() {
     if (navigator.geolocation) { 
@@ -35,7 +36,8 @@ function renderMap()
     // Create a marker
     marker = new google.maps.Marker({
         position: me,
-        title: "Here I Am!"
+        title: "You are here!",
+        icon: "http://google-maps-icons.googlecode.com/files/vegetarian.png"
     });
     marker.setMap(map);
 
@@ -45,38 +47,111 @@ function renderMap()
         infowindow.open(map, marker);
     });
 
-// Calling Google Places API
-    var request = {
-    location: me,
-          radius: '500',
-          types: ['food']
-    };
-    service = new google.maps.places.PlacesService(map);
-    service.search(request, callback);
     }
-
+function findStops() {
+   request.open("GET", "http://mbtamap-cedar.herokuapp.com/mapper/redline.json",
+           true);
+   request.send();
+   request.onreadystatechange = callback;
+}
 // Taken from http://code.google.com/apis/maps/documentation/javascript/places.html
 function callback(results, status)
 {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-        places = results;
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-        }
+    if(request.readyState == 4) {
+        var str = request.responseText;
+        parseRedlineInfo(str);
     }
+}
+function parseRedlineInfo(str) {
+    try {parsed = JSON.parse(str);}
+    catch(err) {
+        alert("parsing error");
+    }
+    finally {
+        createRedlineMarkers();
+    }
+}
+function createRedlineMarkers() {
+    route = [];
+    places = results;
+    stop = new Array(parsed.length);
+    for (i in parsed) {
+        stop[i] = new Object;
+        stop[i].platform = parsed[i].platformkey;
+        stop[i].infoType = parsed[i].informationType;
+        stop[i].time = parsed[i].time;
+        stop[i].timeRem = parsed[i].timeRemaining;
+        stop[i].route = parsed[i].route;
+        getHardCodedInfo(stop[i]);
+        createMarker(stop[i]);
+    }
+     drawRoute();
 }
 
 function createMarker(place)
-{
-    var placeLoc = place.geometry.location;
+{   
+    stopLoc = new google.maps.LatLng(place.lat, place.lng);
+    route.push(stopLoc);
     var marker = new google.maps.Marker({
+        title: "The closest station is " + place.name,
         map: map,
-        position: place.geometry.location
+        icon: "http://google-maps-icons.googlecode.com/files/spa.png",
+        position: stopLoc
     });
+    marker.infoContent = "Arrival Schedule: " + place.time;
 
     google.maps.event.addListener(marker, 'click', function() {
         infowindow.close();
         infowindow.setContent(place.name);
         infowindow.open(map, this);
     });
+}
+
+function drawRoute(){
+    var RedlineRoute = new google.maps.Polyline({
+     clickable: false,
+     map: map,
+     path: route,
+     strokeColor: "#FF0000"
+     });
+}
+
+function findWaldoCarmen() {
+  try {req = new XMLHttpRequest();}
+  catch (err) {
+      alert("Carmen and Waldo's nowhere to be found!");
+      req = null;
+  }
+  finally {
+      if (req == null) {
+          alert("Error requesting");
+      } else {
+          req.open("GET", "http://messagehub.herokuapp.com/a3.json", true);
+          req.send();
+          req.onreadystatechange = showCarmenWaldo;
+      }
+  }
+}
+function showCarmenWaldo() {
+   if(req.readyState == 4) {
+      var str = req.responseText;
+      var where = JSON.parse(str);
+      for (i in where) {
+           pplLoc = new google.maps.LatLng(where[i].loc.latitude,
+           where[i].loc.longitude);
+           var marker = new google.maps.Marker({
+              title: where[i].name + " is here!",
+              map: map,
+              icon: "http://google-maps-icons.googlecode.com/files/vegetarian.png",
+              position: pplLoc
+           });
+           google.maps.event.addListener(marker, 'click', function() {
+            marker.infoContent = "Note: " + where[i].note;
+
+           infowindow.close();
+           infowindow.setContent(where[i].name);
+           infowindow.open(map, this);
+           });
+      }
+   }
 }
---
+++ where/station_info.js
@@ -0,0 +1,139 @@
+ function getHardCodedInfo(station) {
+    switch (station.platform) {
+}
+        case "RALEN":
+            station.name = "Alewife Station";
+            station.lat = 42.39543;
+            station.lng = -71.1425;
+            break;
+        case "RDAVN":
+        case "RDAVS":
+            station.name = "Davis Station";
+            station.lat = 42.39674;
+            station.lng = -71.1218;
+            break;
+        case "RPORN":
+        case "RPORS":
+            station.name = "Porter Station";
+            station.lat = 42.3884;
+            station.lng = -71.1191;
+            break;
+        case "RHARN":
+        case "RHARS":
+            station.name = "Harvard Station";
+            station.lat = 42.37336;
+            station.lng = -71.119;
+            break;
+        case "RCENN":
+        case "RCENS":
+            station.name = "Central Square Station";
+            station.lat = 42.36549;
+            station.lng = -71.1038;
+            break;
+        case "RKENN":
+        case "RKENS":
+            station.name = "Kendall/MIT Station";
+            station.lat = 42.36249079;
+            station.lng = -71.08617653;
+            break;
+		case "RMGHN":
+        case "RMGHS":
+            station.name = "Charles/MGH Station";
+            station.lat =42.361166;
+            station.lng = -71.070628;
+            break;
+        case "RPRKN":
+        case "RPRKS":
+            station.name = "Park St. Station";
+            station.lat = 42.35639457;
+            station.lng = -71.0624242;
+            break;	
+        case "RDTCN":
+        case "RDTCS":
+            station.name = "Downtown Crossing Station";
+            station.lat = 42.355518;
+            station.lng = 	-71.060225;
+            break;	 
+        case "RSOUN":
+        case "RSOUS":
+            station.name = "South Station";
+            station.lat = 42.352271;
+            station.lng = 	-71.055242;
+            break;	 
+        case "RBRON":
+        case "RBROS":
+            station.name = "Broadway Station";
+            station.lat = 42.342622;
+            station.lng = -71.056967;
+            break;	 
+        case "RANDN":
+        case "RANDS":
+            station.name ="Andrew Station" ;
+            station.lat = 42.330154;
+            station.lng = -71.057655;
+            break;	 
+        case "RJFKN":
+        case "RJFKS":
+            station.name = "JFK/UMass Station";
+            station.lat =42.320685;
+            station.lng = -71.052391;
+            break;	 
+        case "RSAVN":
+        case "RSAVS":
+            station.name = "Savin Hill Station";
+            station.lat = 42.31129;
+            station.lng = -71.053331;
+            break;	 
+        case "RFIEN":
+        case "RFIES":
+            station.name = "Fields Corner Station";
+            station.lat = 42.300093;
+            station.lng = -71.061667;
+            break;	 			
+        case "RSHAN":
+        case "RSHAS":
+            station.name = "Shawmut Station";
+            station.lat =42.29312583;
+            station.lng = -71.06573796;
+            break;	 			
+        case "RASHN":
+        case "RASHS":
+            statiOn.name = "Ashmont Station";
+            station.lat =42.284652;
+            station.lng = -71.064489;
+            break;	
+        case "RNQUN":
+        case "RNQUS":
+            station.name = "North Quincy Station";
+            station.lat = 42.275275	
+            station.lng = -71.029583;
+            break;	
+        case "RWOLN":
+        case "RWOLS":
+            station.name = "Wollaston Station";
+            station.lat = 42.2665139;
+            station.lng = 	-71.0203369;
+            break;	
+		case "RQUCN":
+        case "RQUCS":
+            station.name = "Quincy Center Station";
+            station.lat =42.251809 ;
+            station.lng = -71.005409;
+            break;	
+		case "RQUAN":
+        case "RQUAS":
+            station.name = "Quincy Adams Station";
+            station.lat =42.233391;
+            station.lng = -71.007153;
+            break;	
+		case "RBRAS":
+            station.name = "Braintree Station";
+            station.lat = 42.2078543;
+            station.lng = -71.0011385;
+            break;	
+        default:
+            station.name = "Davis Station";
+            station.lat = 42.39674;
+            station.lng = -71.1218;
+            break;
+}
